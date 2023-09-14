717611fe41ea9ace1b620d6a11cff5a5bf0eed8d
SBolivarLoL <mikeldev62@gmail.com>
2023-09-14 15:35:29 -0500

    initial many to many relation Order-Product

--- api/db/models/order.model.js
+++ api/db/models/order.model.js
@@ -33,7 +33,14 @@ class Order extends Model {
   static associate(models) {
     this.belongsTo(models.Customer, {
       as: 'customer'
-    })
+    });
+    // Relation Many to Many with Orders and Products using a ternary table
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId'
+    });
   };
 
   static config(sequelize) {
